625e683772bf97e82cd06b2dac3897d3b95455c0
Shreeyak <ss.shrek7@gmail.com>
2019-03-06 23:12:22 +0530

    added inference class for alphapilot

--- shrek/pytorch-deeplab-xception/dataloaders/alphapilot.py
+++ shrek/pytorch-deeplab-xception/dataloaders/alphapilot.py
@@ -44,20 +44,25 @@ class AlphaPilotSegmentation(Dataset):
 
 
     def __init__(self,
-                 input_dir='data/dataset/train/images',
-                 label_dir='data/dataset/train/labels',
+                 input_dir='',
+                 label_dir='',
                  transform=None,
                  input_only=None,
+                 return_image_name=True,
                  ):
         """
         Args:
             input_dir (str): Path to folder containing the input images.
                 Expects images in them to be in format as in list INPUT_IMG_EXTENSIONS.
+                If initing mock dataloader for alphapilot, pass input_dir as ""
             label_dir (str): Path to folder containing the labels.
                 Expects images in them to be in format as in list LABEL_IMG_EXTENSIONS.
-                If running inference on test set without labels, pass label_dir as None
+                If running inference on test set without labels, pass label_dir as ""
             transform (imgaug transforms): Transforms to be applied to the imgs
             input_only (list, str): List of transforms that are to be applied only to the input img
+            return_image_name (bool) : Controls the number of items that dataloader returns.
+                If True, dataloader will return 3 elements: (img, label, input_filename). Input filename is used for plotting.
+                If False, dataloader will return 2 elements only: (img, label)
         """
         super().__init__()
 
@@ -65,10 +70,12 @@ class AlphaPilotSegmentation(Dataset):
         self.label_dir = label_dir
         self.transform = transform
         self.input_only = input_only
+        self.return_image_name = return_image_name
 
-        assert os.path.isdir(input_dir), 'This directory does not exist: %s' % (input_dir)
-        self.datalist_input = sorted((os.path.join(self.input_dir, img)) for img in os.listdir(self.input_dir)
-                                     if self._isimage(img, self.INPUT_IMG_EXTENSIONS))
+        if self.input_dir:
+            assert os.path.isdir(input_dir), 'This directory does not exist: %s' % (input_dir)
+            self.datalist_input = sorted((os.path.join(self.input_dir, img)) for img in os.listdir(self.input_dir)
+                                        if self._isimage(img, self.INPUT_IMG_EXTENSIONS))
 
         if self.label_dir:
             assert os.path.isdir(label_dir), 'This directory does not exist: %s' % (label_dir)
@@ -80,34 +87,65 @@ class AlphaPilotSegmentation(Dataset):
 
 
     def __len__(self):
-        return len(self.datalist_input)
+        if self.input_dir:
+            dataset_size = len(self.datalist_input)
+        else:
+            dataset_size = 0
+
+        return dataset_size
 
 
     def __getitem__(self, index):
-        _img = self._load_input_image(self.datalist_input[index])
+        '''Returns tensors of image and label. Optionally returns filename
+
+        Args:
+            index (int): the index of the image to be returned
+
+        Returns:
+            Tensor: Tensors of image
+            Tensor: Tensor of labels
+            str (optional) : filename of image
+        '''
+
+        if self.input_dir:
+            _img = self._load_input_image(self.datalist_input[index])
         if self.label_dir:
             _label = self._load_label_image(self.datalist_label[index])
 
-
-        #TODO: Convert label to int at this point, same dtype as _img. See if that helps with the interpolation problem.
-        # Apply processing and convert to Tensor
+        # Apply Augmentations using imgaug
         if self.transform:
             det_tf = self.transform.to_deterministic()
-            _img = det_tf.augment_image(_img)
+
+            if self.input_dir:
+                _img = det_tf.augment_image(_img)
             if self.label_dir:
                 _label = det_tf.augment_image(_label, hooks=ia.HooksImages(activator=self._activator_masks))
 
-        _img = np.ascontiguousarray(_img) # To prevent errors from negative stride, as caused by fliplr()
-        _img_tensor = transforms.ToTensor()(_img)
+        # Convert to Tensor. If images/labels do not exist, return zero tensor.
+        if self.input_dir:
+            _img = np.ascontiguousarray(_img) # To prevent errors from negative stride, as caused by fliplr()
+            _img_tensor = transforms.ToTensor()(_img)
+        else:
+            _img_tensor = torch.zeros((1, 512, 512), dtype=torch.float32)
+
         if self.label_dir:
             _label_tensor = transforms.ToTensor()(_label.astype(np.float32)) # Without conversion of numpy to float, the numbers get normalized
         else:
             _label_tensor = torch.zeros((1, _img_tensor.shape[1], _img_tensor.shape[2]), dtype=torch.float32)
 
-        return _img_tensor, _label_tensor, os.path.splitext(os.path.basename(self.datalist_input[index]))[0]
-        # return _img_tensor, _label_tensor
+        # Return Output
+        if self.return_image_name:
+            output = _img_tensor, _label_tensor, os.path.splitext(os.path.basename(self.datalist_input[index]))[0]
+        else:
+            output = _img_tensor, _label_tensor
+
+        return output
 
     def _activator_masks(self, images, augmenter, parents, default):
+        '''Used by imgaug augmentor hooks to select which augmentations should not be applied to labels as defined
+        by parameter `input_only`.
+        Eg: Augmentations like blur should be applied only to input images, not to labels.
+        '''
         if self.input_only and augmenter.name in self.input_only:
             return False
         else:
